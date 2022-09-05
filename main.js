69842c8690a3043f619e319828a6ca7dd642f25e
Raquibul Islam <shubha360@outlook.com>
2022-09-05 19:51:50 +0600

    changes

---
+++ main.js
@@ -0,0 +1,19 @@
+const googleButton = document.querySelector("#button-google");
+const githubButton = document.querySelector("#button-github");
+const emailButton = document.querySelector("#button-email");
+
+const emailInput = document.querySelector("input[type='email']");
+
+googleButton.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+githubButton.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+githubButton.addEventListener('click', (event) => {
+    event.preventDefault();
+
+
+});
